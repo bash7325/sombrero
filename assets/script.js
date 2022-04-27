cf204438713c631cb85d7343aa2a582bd9c9ee93
Brandon Ashby <bash7325@gmail.com>
2022-04-26 18:08:51 -0600

    adjust weather conditional for testing. fixed styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 let apiKey = "20d3501773a839af3857d2b0374101f6";
 let lat = "40.76";
 let lon = "-111.89";
+let hatDance = document.getElementById("myAudio");
 let sombrero = document.getElementById("sombrero-button");
+let sombreroContainer = document.getElementById("sombrero-container");
+let sombreroImage = document.createElement("img");
+let sombreroWeather = document.createElement("h1");
+let notSombreroWeather = document.createElement("h1");
 
 //get weather from openweathermap.org
 function getWeather() {
@@ -21,27 +26,24 @@ function getWeather() {
       let clouds = data.current.clouds;
       let uvIndex = data.current.uvi;
 
-      if (temp > 50 && clouds < 50 && uvIndex > 2) {
-        //add h1 that says it's sombrero weather
-        let sombreroWeather = document.createElement("h1");
+      if (temp > 50 && clouds < 100 && uvIndex > 0) {
+        //add h1 to the sombrero-container that says it's sombrero weather
+        sombreroWeather.setAttribute("style", "text-align: center");
         sombreroWeather.innerHTML = "It's Sombrero Weather!";
-        sombreroWeather.setAttribute("id", "sombrero-weather");
-        document.body.appendChild(sombreroWeather);
-        //append image of a sombrero
-        let sombreroImage = document.createElement("img");
+        sombreroWeather.classList.add("sombrero-text");
+        sombreroContainer.appendChild(sombreroWeather);
+        //add sombrero image
         sombreroImage.setAttribute("src", "./assets/images/sombrero-image.jpg");
         sombreroImage.setAttribute("id", "sombrero-image");
         document.body.appendChild(sombreroImage);
         //play audio of hat dance
         playAudio();
-      }else
-        {
-            //add h1 that says it's not sombrero weather
-            let notSombreroWeather = document.createElement("h1");
-            notSombreroWeather.innerHTML = "No, It's Not Sombrero Weather";
-            notSombreroWeather.setAttribute("id", "not-sombrero-weather");
-            document.body.appendChild(notSombreroWeather);           
-        }
+      } else {
+        //add h1 that says it's not sombrero weather
+        notSombreroWeather.setAttribute("style", "text-align: center");
+        notSombreroWeather.innerHTML = "No, It's Not Sombrero Weather";
+        sombreroContainer.appendChild(notSombreroWeather);
+      }
     });
 }
 
